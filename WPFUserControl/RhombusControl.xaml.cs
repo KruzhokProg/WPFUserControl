7978d4386a1117bff472590ebe4f07060b50f42a
Nickolay Irtuganov <vongu3@gmail.com>
2020-11-01 15:00:06 +0300

    Добавьте файлы проекта.

---
+++ WPFUserControl/RhombusControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFUserControl
+{
+    /// <summary>
+    /// Логика взаимодействия для RhombusControl.xaml
+    /// </summary>
+    public partial class RhombusControl : UserControl
+    {
+        public string Header { get; set; }
+        public string Description { get; set; }
+        public ImageSource coloredSource { get; set; }
+        public ImageSource BlackWhiteSource =>
+            (coloredSource == null) ? null : new FormatConvertedBitmap((BitmapSource)coloredSource, PixelFormats.Gray8, null, 1);
+
+        public RhombusControl()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Btn_Action_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(Description, Header, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
